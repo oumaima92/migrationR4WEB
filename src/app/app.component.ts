a637f5e570ce003d7be8d7601eaa888e34f0d191
Abouzaid Oumaima <oumaima.abouzaid@inetum.com>
2023-02-13 16:58:37 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit , ViewContainerRef, ViewChild, ComponentFactoryResolver} from '@angular/core';
+import { AT02Component } from './at02/at02.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-tag',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test-app';
+export class AppComponent implements AfterViewInit{  
+  @ViewChild('dynamicInsert', { read: ViewContainerRef }) 
+  dynamicInsert: ViewContainerRef;
+  constructor(
+    private factoryResolver: ComponentFactoryResolver
+
+  ) {
+    
+   }
+   ngAfterViewInit(): void {
+    const componentFactory = this.factoryResolver.resolveComponentFactory(AT02Component);
+    console.log(this.dynamicInsert);
+    this.dynamicInsert.clear();
+    const dynamicComponent = <AT02Component> (this.dynamicInsert.createComponent(componentFactory).instance);
+   }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { TextComponent } from './text/text.component';
+import { AT01Component } from './at01/at01.component';
+import { AT02Component } from './at02/at02.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    TextComponent,
+    AT01Component,
+    AT02Component
   ],
   imports: [
     BrowserModule,
